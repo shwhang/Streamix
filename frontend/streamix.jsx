b5e138ba52758ff0d7ca791d3bb468a872f69464
Sally <wsaehee@gmail.com>
2018-02-10 12:31:15 -0500

    Edit entry file

--- frontend/streamix.jsx
+++ frontend/streamix.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>HELLOO</h1>, root);
-})
+  ReactDOM.render(<h1>Streamix</h1>, root);
+});
