641d197cbed119780cffab2bdbd8f2ea40e20f60
Sally <wsaehee@gmail.com>
2018-02-11 02:44:01 -0500

    Add User Authentication

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,25 @@
 class Api::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_meessages, status: 422
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :email, :password_digest, :session_token, presence: true
+  # TODO: put email validation
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  private
+
+  def self.generate_session_token
+    SecureTandom::urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def reset_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
