1f0342cdad019e25c445ee7ae19e860b30b2acd2
Sally <wsaehee@gmail.com>
2018-02-11 02:53:21 -0500

    Add Session Authentication

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,30 @@
 class Api::SessionsController < ApplicationController
+  # create new session when users login
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      render "api/users/show"
+    else
+      login!(user)
+      redirect_to user_url(user)
+    end
+  end
+
+  # remove sessions when users logout
+  def destroy
+    @user = current_user
+
+    if @user
+      logout!
+      render "api/users/show"
+    else
+      render(
+        json: ["Nobody is signed in."], status: 404
+      )
+    end
+  end
 end
