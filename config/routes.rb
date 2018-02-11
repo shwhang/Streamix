95d2540b3011cd0df747cf30e43cb759276844ff
Sally <wsaehee@gmail.com>
2018-02-11 02:59:57 -0500

    Add user show api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resource :user, only: [:create]
+    resource :user, only: [:create, :show]
     resources :session, only: [:create, :destroy]
   end
 end
