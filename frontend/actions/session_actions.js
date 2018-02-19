3d8e19a7b4cb2c74aef22bd54b705ec16c85cd03
Sally <wsaehee@gmail.com>
2018-02-19 18:44:08 -0500

    Fix typos

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,7 +1,7 @@
 import * as APIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 
 export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
@@ -9,13 +9,13 @@ export const receiveCurrentUser = currentUser => ({
 });
 
 export const receiveSessionErrors = errors => ({
-  type: RECEIVE_ERRORS,
+  type: RECEIVE_SESSION_ERRORS,
   errors
 });
 
 export const signup = user => dispatch => (
   APIUtil.signup(user).then( user => (
-      dispatch(receiveCurrentUser(user));
+      dispatch(receiveCurrentUser(user))
     ), err => (
       dispatch(receiveSessionErrors(err.responseJSON))
     )
@@ -24,7 +24,7 @@ export const signup = user => dispatch => (
 
 export const login = user => dispatch => (
   APIUtil.login(user).then( user => (
-      dispatch(receiveCurrentUser(user));
+      dispatch(receiveCurrentUser(user))
     ), err => (
       dispatch(receiveSessionErrors(err.responseJSON))
     )
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,6 +1,6 @@
 import merge from 'lodash/merge';
 
-import { receieveSessionErrors } from '../actions/sessions_action';
+import { RECEIVE_SESSION_ERRORS } from '../actions/session_actions';
 
 const _nullErrors = Object.freeze({
   sessionErrors: [],
@@ -10,7 +10,7 @@ const errorsReducer = ( state = _nullErrors, action ) => {
   Object.freeze(state);
 
   switch(action.type){
-    case: RECEIVE_SESSION_ERRORS:
+    case RECEIVE_SESSION_ERRORS:
       const sessionErrors = action.session;
       return merge({}, { sessionErrors })
     default:
