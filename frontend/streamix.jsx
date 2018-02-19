b91f48c5962a74d6e6f93e0fcf87e872cb7a9e84
Sally <wsaehee@gmail.com>
2018-02-19 18:08:12 -0500

    Set up store with entry file

--- frontend/streamix.jsx
+++ frontend/streamix.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser }}
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Streamix</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
