13055534e14fae816793750a97defe76c49fe8a1
Sally <wsaehee@gmail.com>
2018-02-19 19:09:10 -0500

    Add splash section

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -7,12 +7,19 @@ class Home extends React.Component{
     super(props);
   }
 
-
   render(){
     //TODO: Add a different view if user is loggedin
     return (
       <div>
         <NavBar />
+
+        <div className="splash-page-section">
+          <h1>See what's Next.</h1>
+          <div className="splash-page-buttons">
+            <Link to="">Sign Up for Free</Link>
+            <Link to="">Demo Login</Link>
+          </div>
+        </div>
       </div>
     )
   }
