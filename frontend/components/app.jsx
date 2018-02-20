efed589ccc6424a4355d51e5aa5fd80fab94e2fc
Sally <wsaehee@gmail.com>
2018-02-19 20:23:44 -0500

    Edit api routes for session form container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomeContainer from './home/home_container';
-import SessionFormContainer from './session/session_form';
+import SessionFormContainer from './session/session_form_container';
 
 const App = () => (
   <div>
