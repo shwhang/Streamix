d2a48c7e275665d45c77641144acdfded4e35224
Sally <wsaehee@gmail.com>
2018-02-19 19:02:23 -0500

    Add Navigation Bar

---
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NavBar extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <nav>
+        <h1>Streamix</h1>
+      </nav>
+    )
+  }
+}
+
+export default NavBar;
