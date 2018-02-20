5db0bfbf2f47c03f557d6c18ad441e9b79ff7af0
Sally <wsaehee@gmail.com>
2018-02-20 12:44:54 -0500

    Edit Protected Route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,6 +4,8 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 import HomeContainer from './home/home_container';
 import SessionFormContainer from './session/session_form_container';
+import ProfileManagement from './profile/profile_management';
+
 
 const App = () => (
   <div>
@@ -11,6 +13,7 @@ const App = () => (
       <Route exact path="/" component={ HomeContainer } />
       <AuthRoute exact path="/signup" component={ SessionFormContainer } />
       <AuthRoute exact path="/login" component={ SessionFormContainer } />
+      <ProtectedRoute exact path="/profiles" component={ProfileManagement} />
     </Switch>
   </div>
 )
--- frontend/util/route_util.js
+++ frontend/util/route_util.js
@@ -13,16 +13,15 @@ const Auth = ({component: Component, path, loggedIn, exact}) => (
   />
 )
 
-const Protected = ({component: Component, path, loggedIn}) => {
+const Protected = ({component: Component, path, loggedIn}) => (
   <Route path={path} render={(props) => (
-      loggedIn ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/login" />
-      )
-    )}
-  />
-}
+    loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/login" />
+    )
+  )}/>
+);
 
 const mapStateToProps = ( ({ session }) => {
   return {
