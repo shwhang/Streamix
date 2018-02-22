ac9788993f8023b9af5ba8a7d9726c02c88c780f
Sally <wsaehee@gmail.com>
2018-02-22 14:42:57 -0500

    Add Profile Form

---
+++ frontend/components/profile/profile_form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import NavBarContainer from '../nav_bar/nav_bar_container';
+
+class ProfileForm extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+}
