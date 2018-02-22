8138ad35175d74ce5409e614ee132daa45486a6c
Sally <wsaehee@gmail.com>
2018-02-22 13:51:19 -0500

    Add profiles to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210173339) do
+ActiveRecord::Schema.define(version: 20180222184415) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
