51f45a4313f2c887144c894311d89c1513ffbd8c
Sally <wsaehee@gmail.com>
2018-02-22 14:29:43 -0500

    Edit Profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,34 @@
 class ProfilesController < ApplicationController
+  def create
+    @profile = Profile.new(profile_params
+    @profile.user_id = current_user.id
+
+    if @profile.save
+      render :show
+    else
+      render json: ['Failed Identity. Try again.']
+    end
+  end
+
+  def index
+    @profile = Profile.all.where(user_id: current_user.id)
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name)
+  end
 end
