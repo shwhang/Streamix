0ab7b6f72f81a07b26f783f696003fe93a812aa1
Sally <wsaehee@gmail.com>
2018-02-28 16:11:50 -0500

    Seed database with avatars

--- app/controllers/api/avatars_controller.rb
+++ app/controllers/api/avatars_controller.rb
@@ -1,5 +1,15 @@
 class Api::AvatarsController < ApplicationController
+  def create
+    @avatar = Avatar.new(avatar_params)
+
+    if @avatar.save
+      render json: ["hello"]
+    else
+      render json: ['Failed avatar']
+    end
+  end
+
   def index
-    
+    @avatars = Avatar.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,49 @@
 
 User.destroy_all
 Profile.destroy_all
+Avatar.destroy_all
+
+#Avatars
+defaultAvatar= Avatar.create(
+  name: "avatar-default",
+  image: File.open('app/assets/images/avatars/default-avatar.png')
+)
+avatar1 = Avatar.create(
+  name: "avatar-1",
+  image: File.open('app/assets/images/avatars/avatar1.png')
+)
+avatar2 = Avatar.create(
+  name: "avatar-2",
+  image: File.open('app/assets/images/avatars/avatar2.png')
+)
+avatar3 = Avatar.create(
+  name: "avatar-3",
+  image: File.open('app/assets/images/avatars/avatar3.png')
+)
+avatar4 = Avatar.create(
+  name: "avatar-4",
+  image: File.open('app/assets/images/avatars/avatar4.png')
+)
+avatar5 = Avatar.create(
+  name: "avatar-5",
+  image: File.open('app/assets/images/avatars/avatar5.png')
+)
+avatar6 = Avatar.create(
+  name: "avatar-6",
+  image: File.open('app/assets/images/avatars/avatar6.png')
+)
+avatar7 = Avatar.create(
+  name: "avatar-7",
+  image: File.open('app/assets/images/avatars/avatar7.png')
+)
+avatar8 = Avatar.create(
+  name: "avatar-8",
+  image: File.open('app/assets/images/avatars/avatar8.png')
+)
+avatar9 = Avatar.create(
+  name: "avatar-9",
+  image: File.open('app/assets/images/avatars/avatar9.png')
+)
 
 #Users
 demoUser = User.create(email: "saehee@gmail.com", password: "shibchill");
@@ -16,11 +59,11 @@ demoUser = User.create(email: "saehee@gmail.com", password: "shibchill");
 demoProfile1 = Profile.create(
   name: "Jake",
   user_id: demoUser.id,
-  # avatar: File.open('app/assets/images/avatars/avatar8.png')
+  avatar_id: avatar5.id
 )
 
 demoProfile2 = Profile.create(
   name: "Marceline",
   user_id: demoUser.id,
-  # avatar: File.open('app/assets/images/avatars/avatar4.png')
+  avatar_id: avatar6.id
 )
