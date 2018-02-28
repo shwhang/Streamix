15874d4633284ab7c7344854b30c7463d763d495
Sally <wsaehee@gmail.com>
2018-02-28 10:01:28 -0500

    Refactor profile form component

--- frontend/components/profile/profile_form.jsx
+++ frontend/components/profile/profile_form.jsx
@@ -5,63 +5,66 @@ import NavBarContainer from '../nav_bar/nav_bar_container';
 class ProfileForm extends React.Component {
   constructor(props) {
     super(props)
-    this.form = {}
+    this.form = {
+      title: "",
+      blurb: "",
+      buttons: ""
+    }
   }
 
-  renderEditForm(){
-    this.form = (
-      <form className="profile-form">
-        <h1>Edit Profile</h1>
-        <p>Add a profile for another person watching Streamix.</p>
-
-        <article className="profile-form-details">
-          <div className="avatar-section"></div>
-
-
-        </article>
-
-
-        <article className="profile-form-buttons">
-
-          <Link to="/profiles">Cancel</Link>
-        </article>
-      </form>
+  renderAddButtons(){
+    return (
+      <article className="profile-form-buttons">
+        <Link to="/profiles">Continue</Link>
+        <Link to="/profiles">Cancel</Link>
+      </article>
     )
   }
 
-  renderAddForm(){
-    this.form = (
-      <form className="profile-form">
-        <h1>Add Profile</h1>
-        <p>Add a profile for another person watching Streamix.</p>
-
-        <article className="profile-form-details">
-          <div className="avatar-section"></div>
-
-
-        </article>
-
-
-        <article className="profile-form-buttons">
-
-          <Link to="/profiles">Cancel</Link>
-        </article>
-      </form>
+  renderEditButtons(){
+    return (
+      <article className="profile-form-buttons">
+        <Link to="/profiles">Save</Link>
+        <Link to="/profiles">Cancel</Link>
+        <Link to="/profiles">Delete Profile</Link>
+      </article>
     )
+  }
+
+  renderFormDetails(){
+    if (this.props.path === "/manage/add") {
+      this.form = {
+        title: "Add Profile",
+        blurb: "Add a profile for another person watching Streamix.",
+        buttons: this.renderAddButtons()
+      }
+    } else {
+      this.form = {
+        title: "Edit Profile",
+        blurb: "",
+        buttons: this.renderEditButtons()
+      }
+    }
   }
 
   render() {
-    if (this.props.path === "/manage/add") {
-      this.renderAddForm();
-    } else {
-      this.renderEditForm();
-    }
+    this.renderFormDetails();
 
     return (
-      <div>
+      <div className="profile-form-container">
         <NavBarContainer />
 
-        {this.form}
+        <form className="profile-form">
+          <h1 className="profile-form-title">{ this.form.title }</h1>
+          <p className="profile-form-blurb">{ this.form.blurb }</p>
+
+          <article className="profile-form-details">
+            <div className="avatar-section"></div>
+          </article>
+
+          { this.form.buttons }
+
+        </form>
       </div>
     )
   }
