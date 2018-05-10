895ac4ac374ce173a0a76c2a2f9c53237214a1b7
Sally <wsaehee@gmail.com>
2018-05-09 23:31:00 -0400

    Add Nav Bar to Genres page

--- frontend/components/library/genre_show.jsx
+++ frontend/components/library/genre_show.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import {Link, withRouter } from 'react-router-dom';
 
+import NavBarContainer from '../nav_bar/nav_bar_container'
 class GenreShow extends React.Component {
   constructor(props) {
     super(props);
 
   }
+  componentWillMount(){
+    this.props.requestAllProfiles();
+  }
 
   render(){
     return (
       <div>
-        <p>Genre</p>
+        <NavBarContainer path={this.props.match.url} allProfiles={this.props.allProfiles}/>
       </div>
     )
   }
--- frontend/components/library/genre_show_container.jsx
+++ frontend/components/library/genre_show_container.jsx
@@ -1,16 +1,24 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
+import { requestAllProfiles } from '../../actions/profile_actions'
 import GenreShow from './genre_show'
 
 export const mapStateToProps = ({genres, profiles}) => ({
   allGenres: genres.allGenres,
-  currentProfile: profiles.currentProfile
+  currentProfile: profiles.currentProfile,
+  allProfiles: profiles.allProfiles
+})
+
+export const mapDispatchToProps = (dispatch, ownProps) => ({
+  requestAllProfiles: () => {
+    return dispatch(requestAllProfiles())
+  }
 })
 
 export default withRouter(
   connect(
     mapStateToProps,
-    null
+    mapDispatchToProps
   )(GenreShow)
 )
