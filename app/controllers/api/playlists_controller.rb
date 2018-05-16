58edf61285b3efa9c327c15eecc184d34f29d68e
Sally <wsaehee@gmail.com>
2018-05-16 11:43:00 -0400

    compile assets

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -1,14 +1,22 @@
 class Api::PlaylistsController < ApplicationController
   def create
+    @playlist = Playlist.new(playlist_params)
 
+    if @playlist.save
+      render :show
+    else
+      render json: ['Playlist Error']
+    end
   end
 
   def show
+    @playlist = Profile.find(params[:id])
 
+    render :show
   end
 
   private
   def playlist_params
-
+    params.require(:playlist).permit(:profile, :medium)
   end
 end
